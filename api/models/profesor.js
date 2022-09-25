4d970b3c4fbc044ff6c5110e8359de18457d5a40
fernandobouchet <fernandobouchet@gmail.com>
2022-09-25 20:37:49 -0300

    Generado modelo 'profesor' * npx sequelize-cli model:generate --name profesor --attributes nombre:string,id_materia:integer

---
+++ api/models/profesor.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const profesor = sequelize.define('profesor', {
+    nombre: DataTypes.STRING,
+    id_materia: DataTypes.INTEGER
+  }, {});
+  profesor.associate = function(models) {
+    // associations can be defined here
+  };
+  return profesor;
+};
