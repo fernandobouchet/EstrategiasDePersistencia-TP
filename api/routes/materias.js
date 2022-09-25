5f73d419e9af56a6e4896d32e344e8081ec34ec9
fernandobouchet <fernandobouchet@gmail.com>
2022-09-25 13:51:56 -0300

    Se agrega la asociación faltante en 'materia'

--- api/routes/materias.js
+++ api/routes/materias.js
@@ -6,6 +6,13 @@ router.get('/', (req, res) => {
   models.materia
     .findAll({
       attributes: ['id_carrera', 'nombre'],
+      include: [
+        {
+          as: 'Carrera',
+          model: models.carrera,
+          attributes: ['id', 'nombre'],
+        },
+      ],
     })
     .then((materias) => res.send(materias))
     .catch(() => res.sendStatus(500));
