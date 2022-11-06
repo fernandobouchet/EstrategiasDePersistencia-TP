2ca04a788f6524208994631a75d4e0cf08bda823
fernandobouchet <fernandobouchet@gmail.com>
2022-11-06 15:28:23 -0300

    Se corrije error en modificacion de nombre de tablas

--- src/migrations/20220925233555-create-profesor.js
+++ src/migrations/20220925233555-create-profesor.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('profesores', {
+    return queryInterface.createTable('profesors', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('profesores');
+    return queryInterface.dropTable('profesors');
   },
 };
